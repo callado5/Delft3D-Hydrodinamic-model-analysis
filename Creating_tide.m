97a62553b85952fd0bc9c948e57be1a345c5f8e6
callado5 <47508053+callado5@users.noreply.github.com>
2023-01-20 11:43:23 -0300

    Update Creating_tide.m
    Para rodar essa rotina é necessário o pacote OET do Delft baixado a parte do repositório do Deltares.
    Nesse caso, temos a retirada da maré em um único ponto a partir de um arquivo _map.nc que nada mais é que o output de um modelo hidrodinâmico.
    Envolvendo a intepolanção de dados no espaço de dados do grid a dados pontuais

--- Creating_tide.m
+++ Creating_tide.m
@@ -1,25 +1,25 @@
-%Criando a marÈ apartir de um modelo existente
+%Para rodar essa rotina √© necess√°rio o pacote OET do delft baixado a parte do reposit√≥rio do Deltares.
+
+%Criando a mar√© apartir de um modelo existente
 addpath('A:\instalaveis_programas\OET\oet_matlab');
 oetsettings;
 
 %% Carregando os FlowElements x e y do Modelo
 
-FlowElem_xcc=nc_varget('C:\Users\lapma\Downloads\pa_ama_medio_map.nc',...
-    'FlowElem_xcc'); %posicao em x
-FlowElem_ycc=nc_varget('C:\Users\lapma\Downloads\pa_ama_medio_map.nc',...
-    'FlowElem_ycc'); % posicao em y
+FlowElem_xcc=nc_varget('*_map.nc','FlowElem_xcc'); %posicao em x
+FlowElem_ycc=nc_varget('*_map.nc','FlowElem_ycc'); % posicao em y
 
 %Criando xq e yq, valores que eu quero resgatar (Como se fosse OBS points).
-m = load('A:\Marco\Modelo_Marinha\tide_points.txt'); %Points (lat,lon)
+m = load('tide_points.txt'); %Points (lat,lon)
 x = FlowElem_xcc;     y = FlowElem_ycc;    
 xq = m(:,1);          yq = m(:,2);  
 
 %Carregando o WaterLevel
-    s1=nc_varget('C:\Users\lapma\Downloads\pa_ama_medio_map.nc','s1',...
+    s1=nc_varget('*_map.nc','s1',...
         [12145 0], [760 71780]); % nivel de superficie livre, [tempoinicial, ?], [Numero de Dados(apartir do ti), Numero de Elementos]
     s1_T = s1'; clear s1;
     %%
-%Gerando para cada ponto xq e yq uma interpolaÁ„o de MarÈ
+%Gerando para cada ponto xq e yq uma interpola√ß√£o de Mar√©
 for i = 1:760 %Como o map varia em uma hora e precisamos de 32 dias pros harmonicos temos 32*24 = 760 = numero de dados
-    sl_new(:,i) = griddata(x,y,s1_T(:,i),xq,yq); %interpolando nivel de ·gua de x e y para xq e yq apartir de um griddata
+    sl_new(:,i) = griddata(x,y,s1_T(:,i),xq,yq); %interpolando nivel de √°gua de x e y para xq e yq apartir de um griddata
 end
