6cb6200f305ae36a0597c2545c640d158789aa3c
Marco Callado <47508053+callado5@users.noreply.github.com>
2026-05-28 15:45:36 -0300

    Update kml2ldb.py
    Now the script accepts polygons and multipolygons

--- kml2ldb.py
+++ kml2ldb.py
@@ -2,7 +2,7 @@
 This code is used to convert a KML to LDB (Delft3D) file. 
 
 Created by Marco Callado in 2026-05-14
-Last edit: 2026-05-14
+Last edit: 2026-05-28
 
 email:
 mavcallado@gmail.com
@@ -16,8 +16,8 @@ from datetime import datetime
 # Opening Kml file                                                         # 
 #==========================================================================#
 
-dir = 'C:\\Users\\mavca\\Downloads\\Curso_DELFT3D\\Codes\\' #Your file directory
-file_name = "Guama" #Put here your file name without its extension
+dir = 'C:\\Users\\mavca\\Downloads\\' #Your file directory
+file_name = "Sao_Caetano" #Put here your file name without its extension
 epsg = 32622 #insert the epsg code for the UTM file
 
 gdf = gpd.read_file(dir+file_name+".kml", driver='KML') #Reading your .kml file
@@ -38,16 +38,50 @@ f.write("* Coordinate System = Cartesian\n")
 
 #Getting data from my .kml file
 for i, row in gdf.iterrows():
-    geom = row.geometry #Reading each row of my .kml
-    coord= np.array(geom.coords) #Getting the kml of each row feature
-    nome = f"Feature_{i+1}"     #Using a name for each
 
-    f.write("*\n")
-    f.write(f"{nome}\n") #Writing the feature name
-    f.write(f"\t{coord.shape[0]}\t{coord.shape[1]-1}\n") #Writing its lat and lon shape
-        
-    for j in range(len(coord)):
-        f.write(f"\t{coord[j,0]:16.7E}\t{coord[j,1]:16.7E}\n") #Writing its lat and lon
+    geom = row.geometry
+    nome = f"Feature_{i+1}"
+
+    # LINESTRING                                                   
+
+    if geom.geom_type == 'LineString':
+
+        coord = np.array(geom.coords)
+
+        f.write("*\n")
+        f.write(f"{nome}\n")
+        f.write(f"\t{coord.shape[0]}\t2\n")
+
+        for j in range(len(coord)):
+            f.write(f"\t{coord[j,0]:16.7E}\t{coord[j,1]:16.7E}\n")
+
+    # POLYGON                                                      
+
+    elif geom.geom_type == 'Polygon':
+
+        coord = np.array(geom.exterior.coords)
+
+        f.write("*\n")
+        f.write(f"{nome}\n")
+        f.write(f"\t{coord.shape[0]}\t2\n")
+
+        for j in range(len(coord)):
+            f.write(f"\t{coord[j,0]:16.7E}\t{coord[j,1]:16.7E}\n")
+
+    # MULTIPOLYGON                                                 
+
+    elif geom.geom_type == 'MultiPolygon':
+
+        for k, poly in enumerate(geom.geoms):
+
+            coord = np.array(poly.exterior.coords)
+
+            f.write("*\n")
+            f.write(f"{nome}_{k+1}\n")
+            f.write(f"\t{coord.shape[0]}\t2\n")
+
+            for j in range(len(coord)):
+                f.write(f"\t{coord[j,0]:16.7E}\t{coord[j,1]:16.7E}\n")
 
 f.close() #Closing ldb file
 
